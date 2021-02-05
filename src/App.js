ad3a521a3f84b5969ce524b041b7f1362e50c695
DiegoMutre <tysonmutre10@gmail.com>
2021-02-05 06:35:17 -0500

    feat: Fetch the api with axios and using an useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 
 function App() {
     // State for search terms
     const [searchTerms, setSearchTerms] = useState({});
+
+    // Fetch the api
+    useEffect(() => {
+        if (Object.keys(searchTerms).length > 1) {
+            const getLyrics = async () => {
+                const { artist, song } = searchTerms;
+                const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+                const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+
+                const [lyrics, info] = await Promise.all([
+                    axios(url),
+                    axios(url2),
+                ]);
+                console.log(lyrics, info);
+            };
+            getLyrics();
+        }
+    }, [searchTerms]);
 
     return (
         <>
