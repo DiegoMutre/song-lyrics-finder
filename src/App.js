48a9db4f20f9638e36548a0cbec03657864be686
DiegoMutre <tysonmutre10@gmail.com>
2021-02-05 06:21:09 -0500

    feat: Create state for App component.
    * Send state modifier function to Form component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Form from "./components/Form";
 
 function App() {
+    // State for search terms
+    const [searchTerms, setSearchTerms] = useState({});
+
     return (
         <>
-            <Form />
+            <Form setSearchTerms={setSearchTerms} />
         </>
     );
 }
