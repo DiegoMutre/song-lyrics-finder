35a9a799545bb4dc0e5bc684a641a8614600aa1f
DiegoMutre <tysonmutre10@gmail.com>
2021-02-05 06:54:55 -0500

    feat: Create ArtistInfo component.
    * Add a new folder to .gitignore file.

---
+++ src/components/ArtistInfo.js
@@ -0,0 +1,55 @@
+import PropTypes from "prop-types";
+
+const ArtistInfo = ({
+    artistInfo: {
+        strArtistThumb,
+        strGenre,
+        strBiographyES,
+        strFacebook,
+        strTwitter,
+        strLastFMChart,
+    },
+}) => {
+    return (
+        <div className="card border-light">
+            <div className="card-header bg-primary text-light font-weight-bold">
+                Artist Information
+            </div>
+            <div className="card-body">
+                <img src={strArtistThumb} alt="Artist Logo" />
+                <p className="card-text">Genre: {strGenre}</p>
+                <h2 className="card-text">Biography:</h2>
+                <p className="card-text">{strBiographyES}</p>
+                <p className="card-text">
+                    <a
+                        href={`https://${strFacebook}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a
+                        href={`https://${strTwitter}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a
+                        href={`${strLastFMChart}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <i className="fab fa-lastfm"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+ArtistInfo.propTypes = {
+    artistInfo: PropTypes.object.isRequired,
+};
+
+export default ArtistInfo;
